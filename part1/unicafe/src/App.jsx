d53b66c5bae5ef8c36be3f1893f9361ac97d914f
Loren <loren@tasiva.com>
2025-03-21 13:05:09 +0100

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,6 +13,9 @@ const Statistics = (props) => (
     <p>good {props.good}</p>
     <p>neutral {props.neutral}</p>
     <p>bad {props.bad}</p>
+    <p>all {props.good + props.bad + props.neutral}</p>
+    <p>average {(props.good + props.bad + props.neutral)/3}</p>
+    <p>positive {props.good / (props.good + props.bad + props.neutral)} %</p>
   </>
 
 )
