3c48b0bef5039dc722a4492d5ee5b4492bda99b1
Loren <loren@tasiva.com>
2025-03-26 08:51:38 +0100

    2.8

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -2,32 +2,43 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     console.log(persons)
     console.log(personObject.name);
-    
-    if (!persons.map(person => person.name).includes(personObject.name)){
+
+    if (!persons.map(person => person.name).includes(personObject.name)) {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
-    else{
+    else {
       alert(`${personObject.name} is already added to phonebook`)
     }
-    
+
   }
-  
+
   const handleNameChange = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     setNewName(event.target.value)
-    
+
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,16 +46,18 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person =>
-          <div key={person.name}>{person.name}</div>
-        )}
+      {persons.map(person =>
+        <div key={person.name}>{person.name} {person.number}</div>
+      )}
     </div>
   )
 }
