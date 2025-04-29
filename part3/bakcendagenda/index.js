45756c11fd7dabd588630ed70c2881cebd2ba2ed
Loren <loren@tasiva.com>
2025-04-29 11:17:15 +0200

    3.5 y 3.6

--- part3/bakcendagenda/index.js
+++ part3/bakcendagenda/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -49,6 +51,36 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const getRandomInt = (max) => {
+    
+    return Math.floor(Math.random() * max)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+    if (!body.name || !body.number){
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    else if (persons.some(p => p.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const id = getRandomInt(1000)
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
